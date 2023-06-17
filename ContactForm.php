7298bfc8e75e940c4018c18032e4f5858c6f972c
Mujeeb Rahman <92824203+mujjurahman@users.noreply.github.com>
2023-06-17 20:17:04 +0530

    Update ContactForm.php

--- ContactForm.php
+++ ContactForm.php
@@ -43,21 +43,79 @@ class ContactForm extends FormBase {
   /**
    * {@inheritdoc}
    */
-  public function buildForm(array $form, FormStateInterface $form_state) {
-    // To be continued
-  }
+// Building the form
+public function buildForm(array $form, FormStateInterface $form_state) {
 
-  /**
-   * {@inheritdoc}
-   */
-  public function validateForm(array &$form, FormStateInterface $form_state) {
-    // To be continued
-  }
+    $form['file_upload'] = [
+        '#type' => 'managed_file',
+        '#title' => $this->t('Upload file'),
+        '#upload_location' => 'public://custom_module/',
+        '#required' => TRUE,
+        '#description' => $this->t('Only files with the following extensions are allowed: xlsx xls csv'),
+        '#upload_validators' => [
+            'file_validate_extensions' => ['xlsx xls csv'],
+        ],
+    ];
 
-  /**
-   * {@inheritdoc}
-   */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
-    // To be continued
-  }
+    $form['submit'] = [
+        '#type' => 'submit',
+        '#value' => $this->t('Submit'),
+    ];
+
+    return $form;
+}
+
+// Validate form
+public function validateForm(array &$form, FormStateInterface $form_state) {
+    // Check if the uploaded file extension is csv
+    $all_files = $form_state->getValue(['file_upload'], []);
+    if (!empty($all_files)) {
+        /* Fetch the array of the file stored temporarily in database */
+        $current_file = File::load( $all_files[0] );
+        // Check if file is not empty
+        if ($current_file !== null) {
+            $file_extension = $current_file->getFileUri();
+            $file_extension = pathinfo($file_extension, PATHINFO_EXTENSION);
+            if ($file_extension !== 'csv') {
+                // If file is not a CSV, raise an error
+                $form_state->setErrorByName('file_upload', $this->t('Only CSV files are allowed.'));
+            }
+        }
+    }
+}
+
+// Submit form
+public function submitForm(array &$form, FormStateInterface $form_state) {
+    $connection = Database::getConnection();
+    $all_files = $form_state->getValue(['file_upload'], []);
+    if (!empty($all_files)) {
+        /* Load the object of the file by it's fid */
+        $file = File::load( $all_files[0] );
+        /* Set the status flag permanent of the file object */
+        $file->setPermanent();
+        /* Save the file in database */
+        $file->save();
+
+        $handle = fopen($file->getFileUri(), 'r');
+        $header = fgetcsv($handle);
+        $queries = [];
+        while (($data = fgetcsv($handle)) !== FALSE) {
+            $row = array_combine($header, $data);
+            $queries[] = [
+                'produit' => $row['Produit'],
+                'aire_therapeutique' => $row['Aire thérapeutique'],
+                'department' => $row['Département'],
+                'rmr_adresse_email' => $row['RMR adresse email'],
+                'backup_adresse_email' => $row['Backup adresse email'],
+            ];
+        }
+        fclose($handle);
+        // Delete existing data in table
+        $connection->truncate('custom_table')->execute();
+        // Insert new data
+        foreach($queries as $query) {
+            $connection->insert('custom_table')->fields($query)->execute();
+        }
+        $this->messenger()->addMessage($this->t('The CSV file has been successfully imported.'));
+    }
 }
